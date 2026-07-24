b2d5d0ec576c9fc71e75d9d6f7774c29ba4edd45
Keshav Bhardwaj <keshavbhardwaj2800@gmail.com>
2026-07-24 22:42:35 +0530

    DAY 5

---
+++ Day005.py
@@ -0,0 +1,14 @@
+# ---------PYTHON FUNCTION---------------
+
+def greater(a,b):
+    if(a>b):
+        print("first is greater")
+    else:
+        print("Second is greater")
+
+greater(14,12)
+
+# Use of pass in functios
+
+def lesser(a,b):
+    pass
